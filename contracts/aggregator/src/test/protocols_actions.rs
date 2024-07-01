b68dad872a4c1b037477f270541e7086ff0f253c
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-07-01 20:53:33 +0800

    test overwritten protocol

--- contracts/aggregator/src/test/protocols_actions.rs
+++ contracts/aggregator/src/test/protocols_actions.rs
@@ -1,7 +1,7 @@
 use soroban_sdk::{Address, Vec, vec, String, testutils::Address as _};
 
 use crate::error::AggregatorError;
-use crate::test::{SoroswapAggregatorTest, create_protocols_addresses};
+use crate::test::{SoroswapAggregatorTest, create_protocols_addresses, create_soroswap_router};
 use crate::models::{ProxyAddressPair};
 
 pub fn new_update_protocols_addresses(test: &SoroswapAggregatorTest) -> Vec<ProxyAddressPair> {
@@ -13,8 +13,19 @@ pub fn new_update_protocols_addresses(test: &SoroswapAggregatorTest) -> Vec<Prox
     ]
 }
 
+// Create new soroswap router to overwrite the porevious
+pub fn update_overwrite_soroswap_protocols_addresses(test: &SoroswapAggregatorTest) -> Vec<ProxyAddressPair> {
+    let new_router = create_soroswap_router(&test.env);
+    vec![&test.env,
+        ProxyAddressPair {
+            protocol_id: String::from_str(&test.env, "soroswap"),
+            address: new_router,
+        },
+    ]
+}
+
 #[test]
-fn test_update_protocols() {
+fn test_update_protocols_add_new() {
     let test = SoroswapAggregatorTest::setup();
 
     //Initialize aggregator
@@ -29,6 +40,32 @@ fn test_update_protocols() {
     let result = test.aggregator_contract.update_protocols(&update_aggregator_addresses);
 
     assert_eq!(result, ());
+}
+
+// test that soroswaop protocol is indeed overwriten with new router addresws
+
+#[test]
+fn test_update_protocols_overwrite() {
+    let test = SoroswapAggregatorTest::setup();
+
+    //Initialize aggregator
+    let initialize_aggregator_addresses = create_protocols_addresses(&test);
+    test.aggregator_contract.initialize(&test.admin, &initialize_aggregator_addresses);
+
+    // check initial protocol values
+    let protocols = test.aggregator_contract.get_protocols();
+    assert_eq!(protocols, initialize_aggregator_addresses);
+
+    // generate new router address and protocol addresses
+    let update_aggregator_addresses = update_overwrite_soroswap_protocols_addresses(&test);
+    // check that router address are different
+    assert_ne!(update_aggregator_addresses[0].address, initialize_aggregator_addresses[0].address);
+
+    test.aggregator_contract.update_protocols(&update_aggregator_addresses);
+
+    // check that protocol values are updated
+    let updated_protocols = test.aggregator_contract.get_protocols();
+    assert_eq!(updated_protocols, update_aggregator_addresses);
 }
 
 #[test]
