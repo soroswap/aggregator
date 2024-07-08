6a6b12ffb0a6ad34d0a26dc44d91566f96a80d43
coderipper <hello@joaquinsoza.dev>
2024-07-08 15:53:00 +0700

    new soroswap-aggregator-workspace

--- contracts/aggregator/src/adapter.rs
+++
@@ -1,5 +0,0 @@
-soroban_sdk::contractimport!(
-    file =
-        "../adapters/soroswap/target/wasm32-unknown-unknown/release/soroswap_adapter.optimized.wasm"
-);
-pub type SoroswapAggregatorAdapterClient<'a> = Client<'a>;
