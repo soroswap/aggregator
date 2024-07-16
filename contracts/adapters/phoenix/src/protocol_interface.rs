92e783a44d8040ecdea0390fada51f67d5027251
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-07-16 18:04:41 +0700

    add comments

--- contracts/adapters/phoenix/src/protocol_interface.rs
+++ contracts/adapters/phoenix/src/protocol_interface.rs
@@ -9,18 +9,34 @@ soroban_sdk::contractimport!(
 );
 pub type PhoenixMultihopClient<'a> = Client<'a>;
 
-fn convert_to_swaps(e: &Env, addresses: &Vec<Address>) -> Vec<Swap> {
+fn convert_to_swaps(e: &Env, path: &Vec<Address>) -> Vec<Swap> {
     let mut swaps = Vec::new(e);
 
-    // Iterate through the addresses, creating a Swap for each pair
-    // Skip the last address since it cannot be an offer_asset without a corresponding ask_asset
+    // Iterate through the addresses in the path, creating a Swap object for each pair
+    // If path is [token0, token1, token2, token3], swaps should be
+    // swap_0 = Swap{
+    //     offer_asset: token0,
+    //     ask_asset: token1,
+    //     ask_asset_min_amount: None,
+    // },
+    // swap_1 = Swap{
+    //     offer_asset: token1,
+    //     ask_asset: token2,
+    //     ask_asset_min_amount: None,
+    // },
+    // swap_2 = Swap{
+    //     offer_asset: token2,
+    //     ask_asset: token3,
+    //     ask_asset_min_amount: None,
+    // }
+
     for i in 0..(addresses.len() - 1) {
         let offer_asset = addresses.get(i).expect("Failed to get offer asset");
-        let ask_asset = addresses.get(i + 1).expect("Failed to get ask asset");
+        let ask_asset = addresses.get(i + 1).expect("Failed to get ask a    sset");
 
         swaps.push_back(Swap {
-          ask_asset: ask_asset.clone(),
-          offer_asset: offer_asset.clone(),
+          offer_asset: offer_asset.clone(), // asset being sold (token_in)
+          ask_asset: ask_asset.clone(), // asset buying (token_out)
           ask_asset_min_amount: None,
         });
     }
@@ -42,8 +58,13 @@ pub fn protocol_swap_exact_tokens_for_tokens(
     let phoenix_multihop_client = PhoenixMultihopClient::new(&e, &phoenix_multihop_address);
     let operations = convert_to_swaps(e, path);
 
-    // TODO: CHECK AND TEST
-    phoenix_multihop_client.swap(&to, &operations, &None, &amount_in);
+    // By using max_spread_bps = None, the Phoenix LP will use the maximum allowed slippage
+    // amount_in is the amount being sold of the first token in the operations.
+    phoenix_multihop_client.swap(
+        &to, // recipient: Address, 
+        &operations, // operations: Vec<Swap>,
+        &None, // max_spread_bps: Option<i64>.
+        &amount_in); //amout: i128. Amount being sold. Input from the user,
 
     // Returning empty array (should check phoenix response if it return amounts, apparently it doesnt)
     Ok(vec![&e])
