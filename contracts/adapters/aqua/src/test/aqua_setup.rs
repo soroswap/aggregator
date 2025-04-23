5676e90c5eeb52df4a73907089f7d1fac3b0338c
esteblock <estebaniglesiasmanriquez@gmail.com>
2025-04-23 08:39:46 -0300

    upgrade aqua contracts

--- contracts/adapters/aqua/src/test/aqua_setup.rs
+++ contracts/adapters/aqua/src/test/aqua_setup.rs
@@ -14,53 +14,24 @@ use soroban_sdk::{
     },
 };
 
-/* *************  AQUA FACTORY  *************  */
+/* *************  AQUA FACTORY AND ROUTER IS THE SAME CONTRACT  *************  */
 
 #[allow(clippy::too_many_arguments)]
-pub mod factory {
+pub mod router {
     soroban_sdk::contractimport!(
-        file = "./aqua_contracts/aqua_factory.wasm"
+        file = "./aqua_contracts/soroban_liquidity_pool_router_contract.wasm"
     );
 }
-use crate::test::aqua_setup::factory::{LiquidityPoolInitInfo, StakeInitInfo, TokenInitInfo};
 
-pub fn deploy_factory_contract(e: &Env, admin: & Address) -> Address {
-    let factory_wasm = e.deployer().upload_contract_wasm(factory::WASM);
+pub fn deploy_router_contract(e: &Env, admin: & Address) -> Address {
+    let router_wasm = e.deployer().upload_contract_wasm(factory::WASM);
     let salt = Bytes::new(&e.clone());
     let salt = e.crypto().sha256(&salt);
 
-    e.deployer().with_address(admin.clone(), salt).deploy(factory_wasm)
+    e.deployer().with_address(admin.clone(), salt).deploy(router_wasm)
 }
 
-pub use factory::Client as AquaFactory;
-
-/* *************  MULTIHOP  *************  */
-#[allow(clippy::too_many_arguments)]
-pub mod multihop {
-    soroban_sdk::contractimport!(file = "./aqua_contracts/aqua_multihop.wasm");
-    pub type MultihopClient<'a> = Client<'a>;
-}
-pub use multihop::MultihopClient; 
-
-pub fn install_multihop_wasm(env: &Env) -> BytesN<32> {
-    soroban_sdk::contractimport!(
-        file = "./aqua_contracts/aqua_multihop.wasm"
-    );
-    env.deployer().upload_contract_wasm(WASM)
-}
-pub fn deploy_multihop_contract<'a>(
-    env: &Env,
-    admin: impl Into<Option<Address>>,
-    factory: &Address,
-) -> MultihopClient<'a> {
-    let admin = admin.into().unwrap_or(Address::generate(env));
-
-    let multihop_address = &env.register_contract_wasm(None, multihop::WASM);
-    let multihop = MultihopClient::new(env, multihop_address); 
-
-    multihop.initialize(&admin, factory);
-    multihop
-}
+pub use router::Client as AquaRouter;
 
 /* *************  TOKEN  *************  */
 
@@ -72,20 +43,6 @@ pub mod token_contract {
 
 pub use token_contract::Client as TokenClient;
 
-// pub fn create_token_contract_with_metadata<'a>(
-//     env: &Env,
-//     admin: &Address,
-//     decimals: u32,
-//     name: String,
-//     symbol: String,
-//     amount: i128,
-// ) -> TokenClient<'a> {
-//     let token =
-//         TokenClient::new(env, &env.register_contract_wasm(None, token_contract::WASM));
-//     token.initialize(admin, &decimals, &name.into_val(env), &symbol.into_val(env));
-//     token.mint(admin, &amount);
-//     token
-// }
 
 pub fn install_token_wasm(env: &Env) -> BytesN<32> {
     soroban_sdk::contractimport!(
@@ -99,206 +56,196 @@ pub fn deploy_token_contract<'a>(env: & Env, admin: & Address) -> token_contract
 }
 
 
-/* *************  LP CONTRACT  *************  */
+/* *************  POOL CONTRACTS  *************  */
 
-#[allow(clippy::too_many_arguments)]
-pub mod lp_contract {
+pub mod standard_pool {
     soroban_sdk::contractimport!(
-        file = "./aqua_contracts/aqua_pool.wasm"
+        file = "./aqua_contracts/soroban_liquidity_pool_contract.wasm"
     );
 }
 
-pub fn install_lp_contract(env: &Env) -> BytesN<32> {
-    env.deployer().upload_contract_wasm(lp_contract::WASM)
+pub fn install_liq_pool_hash(e: &Env) -> BytesN<32> {
+    e.deployer().upload_contract_wasm(standard_pool::WASM)
 }
 
-
-/* *************  STAKE  *************  */
-
-#[allow(clippy::too_many_arguments)]
-pub fn install_stake_wasm(env: &Env) -> BytesN<32> {
+pub mod stableswap_pool {
     soroban_sdk::contractimport!(
-        file = "./aqua_contracts/aqua_stake.wasm"
-    );
-    env.deployer().upload_contract_wasm(WASM)
-}
-
-
-pub fn deploy_and_mint_tokens<'a>(
-    env: & Env,
-    admin: & Address,
-    amount: i128,
-) -> token_contract::Client<'a> {
-    let token = deploy_token_contract(&env, &admin);
-    token.mint(&admin, &amount);
-    token
-}
-
-
-pub fn deploy_and_initialize_factory<'a>(env: &Env, admin: Address) -> AquaFactory<'a> {
-    let factory_addr = deploy_factory_contract(&env, &admin.clone());
-    let factory_client = AquaFactory::new(env, &factory_addr);
-    let multihop_wasm_hash = install_multihop_wasm(env);
-    let whitelisted_accounts = vec![env, admin.clone()];
-
-    let lp_wasm_hash = install_lp_contract(env);
-    let stake_wasm_hash = install_stake_wasm(env);
-    let token_wasm_hash = install_token_wasm(env);
-
-    factory_client.initialize(
-        &admin.clone(),
-        &multihop_wasm_hash,
-        &lp_wasm_hash,
-        &stake_wasm_hash,
-        &token_wasm_hash,
-        &whitelisted_accounts,
-        &10u32,
-    );
-    factory_client
-}
-
-#[allow(clippy::too_many_arguments)]
-pub fn deploy_and_initialize_lp(
-    env: &Env,
-    factory: &AquaFactory,
-    admin: Address,
-    mut token_a: Address,
-    mut token_a_amount: i128,
-    mut token_b: Address,
-    mut token_b_amount: i128,
-    fees: Option<i64>,
-) {
-    // 2. create liquidity pool from factory
-
-    if token_b < token_a {
-        std::mem::swap(&mut token_a, &mut token_b);
-        std::mem::swap(&mut token_a_amount, &mut token_b_amount);
-    }
-
-    let token_init_info = TokenInitInfo {
-        token_a: token_a.clone(),
-        token_b: token_b.clone(),
-    };
-    let stake_init_info = StakeInitInfo {
-        min_bond: 10i128,
-        min_reward: 5i128,
-        manager: Address::generate(env),
-        max_complexity: 10u32,
-    };
-
-    let lp_init_info = LiquidityPoolInitInfo {
-        admin: admin.clone(),
-        fee_recipient: admin.clone(),
-        max_allowed_slippage_bps: 5000,
-        max_allowed_spread_bps: 500,
-        swap_fee_bps: fees.unwrap_or(0i64),
-        max_referral_bps: 5_000,
-        token_init_info,
-        stake_init_info,
-    };
-
-    let lp = factory.create_liquidity_pool(
-        &admin.clone(),
-        &lp_init_info,
-        &String::from_str(env, "Pool"),
-        &String::from_str(env, "PHO/XLM"),
-    );
-
-    let lp_client = lp_contract::Client::new(env, &lp);
-    lp_client.provide_liquidity(
-        &admin.clone(),
-        &Some(token_a_amount),
-        &None,
-        &Some(token_b_amount),
-        &None,
-        &None::<i64>,
+        file = "./aqua_contracts/soroban_liquidity_pool_stableswap_contract.wasm"
     );
 }
 
+pub fn install_stableswap_liq_pool_hash(e: &Env) -> BytesN<32> {
+    e.deployer().upload_contract_wasm(stableswap_pool::WASM)
+}
+
+mod pool_plane {
+    soroban_sdk::contractimport!(
+        file =
+            "./aqua_contracts/soroban_liquidity_pool_plane_contract.wasm"
+    );
+}
+
+pub fn create_plane_contract<'a>(e: &Env) -> pool_plane::Client<'a> {
+    pool_plane::Client::new(e, &e.register(pool_plane::WASM, ()))
+}
+
+mod liquidity_calculator {
+    soroban_sdk::contractimport!(
+        file =
+            "./aqua_contracts/soroban_liquidity_pool_liquidity_calculator_contract.wasm"
+    );
+}
+
+pub fn create_liquidity_calculator_contract<'a>(e: &Env) -> liquidity_calculator::Client<'a> {
+    liquidity_calculator::Client::new(e, &e.register(liquidity_calculator::WASM, ()))
+}
+
+mod reward_boost_feed {
+    soroban_sdk::contractimport!(
+        file = "./aqua_contracts/soroban_locker_feed_contract.wasm"
+    );
+}
+
+pub(crate) fn create_reward_boost_feed_contract<'a>(
+    e: &Env,
+    admin: &Address,
+    operations_admin: &Address,
+    emergency_admin: &Address,
+) -> reward_boost_feed::Client<'a> {
+    reward_boost_feed::Client::new(
+        e,
+        &e.register(
+            reward_boost_feed::WASM,
+            reward_boost_feed::Args::__constructor(admin, operations_admin, emergency_admin),
+        ),
+    )
+}
 
 pub struct AquaTest<'a> {
     pub env: Env,
-    pub multihop_client: MultihopClient<'a>,
-    pub factory_client: AquaFactory<'a>,
-    pub token_0: TokenClient<'a>,
-    pub token_1: TokenClient<'a>,
-    pub token_2: TokenClient<'a>,
-    pub token_3: TokenClient<'a>,
-    pub user: Address,
-    pub admin: Address
+
+    pub admin: Address,
+
+    pub tokens: [test_token::Client<'a>; 4],
+    pub reward_token: test_token::Client<'a>,
+    pub reward_boost_token: test_token::Client<'a>,
+    pub reward_boost_feed: reward_boost_feed::Client<'a>,
+
+    pub router: LiquidityPoolRouterClient<'a>,
+
+    pub emergency_admin: Address,
+    pub rewards_admin: Address,
+    pub operations_admin: Address,
+    pub pause_admin: Address,
+    pub emergency_pause_admin: Address,
 }
 
 impl<'a> AquaTest<'a> {
     pub fn aqua_setup() -> Self {
         let env = Env::default();
         env.mock_all_auths();
-        env.budget().reset_unlimited();
+        env.cost_estimate().budget().reset_unlimited();
 
+        // let user = Address::generate(&env);
+        // let initial_admin_balance = 10_000_000i128;
+        
+        let mut tokens = std::vec![
+            create_token_contract(&env, &admin).address,
+            create_token_contract(&env, &admin).address,
+            create_token_contract(&env, &admin).address,
+            create_token_contract(&env, &admin).address,
+        ];
+        tokens.sort();
+        let tokens = [
+            test_token::Client::new(&env, &tokens[0]),
+            test_token::Client::new(&env, &tokens[1]),
+            test_token::Client::new(&env, &tokens[2]),
+            test_token::Client::new(&env, &tokens[3]),
+        ];
+
+        let reward_admin = Address::generate(&env);
         let admin = Address::generate(&env);
-        let user = Address::generate(&env);
+        let payment_for_creation_address = Address::generate(&env);
 
-        let initial_admin_balance = 10_000_000i128;
+        let reward_token = create_token_contract(&env, &reward_admin);
+        let reward_boost_token = create_token_contract(&env, &reward_admin);
 
-        let token_0 = deploy_and_mint_tokens(&env, &admin, initial_admin_balance);
-        let token_1 = deploy_and_mint_tokens(&env, &admin, initial_admin_balance);
-        let token_2 = deploy_and_mint_tokens(&env, &admin, initial_admin_balance);
-        let token_3 = deploy_and_mint_tokens(&env, &admin, initial_admin_balance);
+        let pool_hash = install_liq_pool_hash(&env);
+        let token_hash = install_token_wasm(&env);
+        let router = create_liqpool_router_contract(&env);
 
-        // 1. deploy factory
-        let factory_client = deploy_and_initialize_factory(&env.clone(), admin.clone());
+        router.init_admin(&admin);
 
-        deploy_and_initialize_lp(
+        let rewards_admin = soroban_sdk::Address::generate(&env);
+        let operations_admin = soroban_sdk::Address::generate(&env);
+        let pause_admin = soroban_sdk::Address::generate(&env);
+        let emergency_pause_admin = soroban_sdk::Address::generate(&env);
+        let reward_boost_feed = create_reward_boost_feed_contract(
             &env,
-            &factory_client,
-            admin.clone(),
-            token_0.address.clone(),
-            1_000_000,
-            token_1.address.clone(),
-            1_000_000,
-            None,
+            &admin,
+            &operations_admin,
+            &emergency_pause_admin,
         );
-        deploy_and_initialize_lp(
-            &env,
-            &factory_client,
-            admin.clone(),
-            token_1.address.clone(),
-            1_000_000,
-            token_2.address.clone(),
-            1_000_000,
-            None,
-        );
-        deploy_and_initialize_lp(
-            &env,
-            &factory_client,
-            admin.clone(),
-            token_2.address.clone(),
-            1_000_000,
-            token_3.address.clone(),
-            1_000_000,
-            None,
+        router.set_privileged_addrs(
+            &admin,
+            &rewards_admin,
+            &operations_admin,
+            &pause_admin,
+            &Vec::from_array(&env, [emergency_pause_admin.clone()]),
         );
 
-        // Setup multihop
-        let multihop_client = deploy_multihop_contract(&env, admin.clone(), &factory_client.address);
-        token_0.mint(&user, &1000i128);
+        router.set_pool_hash(&admin, &pool_hash);
+        router.set_stableswap_pool_hash(&admin, &install_stableswap_liq_pool_hash(&env));
+        router.set_token_hash(&admin, &token_hash);
+        router.set_reward_token(&admin, &reward_token.address);
+        router.configure_init_pool_payment(
+            &admin,
+            &reward_token.address,
+            &1_0000000,
+            &1_0000000,
+            &payment_for_creation_address,
+        );
+        router.set_reward_boost_config(
+            &admin,
+            &reward_boost_token.address,
+            &reward_boost_feed.address,
+        );
 
-        // Check initial user value of every token:
+        let emergency_admin = Address::generate(&env);
+        router.commit_transfer_ownership(
+            &admin,
+            &Symbol::new(&env, "EmergencyAdmin"),
+            &emergency_admin,
+        );
+        router.apply_transfer_ownership(&admin, &Symbol::new(&env, "EmergencyAdmin"));
 
-        assert_eq!(token_0.balance(&user), 1000i128);
-        assert_eq!(token_1.balance(&user), 0i128);
-        assert_eq!(token_2.balance(&user), 0i128);
-        assert_eq!(token_3.balance(&user), 0i128);
+        let plane = create_plane_contract(&env);
+        router.set_pools_plane(&admin, &plane.address);
+
+        let liquidity_calculator = create_liquidity_calculator_contract(&env);
+        liquidity_calculator.init_admin(&admin);
+        liquidity_calculator.set_pools_plane(&admin, &plane.address);
+        router.set_liquidity_calculator(&admin, &liquidity_calculator.address);
+
+
+        // assert_eq!(token_0.balance(&user), 1000i128);
+        // assert_eq!(token_1.balance(&user), 0i128);
+        // assert_eq!(token_2.balance(&user), 0i128);
+        // assert_eq!(token_3.balance(&user), 0i128);
 
     AquaTest {
-            env: env.clone(),
-            multihop_client,
-            factory_client,
-            token_0,
-            token_1,
-            token_2,
-            token_3,
-            user,
-            admin: admin.clone()
+        env,
+        admin,
+        tokens,
+        reward_token,
+        router,
+        emergency_admin,
+        rewards_admin,
+        operations_admin,
+        pause_admin,
+        emergency_pause_admin,
+        reward_boost_token,
+        reward_boost_feed,
         }
     }
 }
